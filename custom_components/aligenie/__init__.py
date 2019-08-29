16ecc2773953d94c410ab601e4a8141283f196bd
Yonsm <Yonsm@qq.com>
2019-08-29 16:38:47 +0800

    Fix bug on check alias in aligenie

--- custom_components/aligenie/__init__.py
+++ custom_components/aligenie/__init__.py
@@ -370,7 +370,7 @@ def guessDeviceName(entity_id, attributes, places, aliases):
 
     # Name validation
     for alias in aliases:
-        if name == aliases['key'] or name in aliases['value']:
+        if name == alias['key'] or name in alias['value']:
             return name
 
     _LOGGER.error(
