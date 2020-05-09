3b86ef1946e52fc7f92f2f3b72f57a4d026b54b1
Yonsm <Yonsm@qq.com>
2020-05-09 14:54:01 +0800

    Install HomeAssistant on CoreElec

--- extra/setup.sh
+++ extra/setup.sh
@@ -118,3 +118,28 @@ alias hasste='systemctl stop homeassistant; hass'
 alias hassup='systemctl stop homeassistant; pip3 install homeassistant --upgrade; systemctl start homeassistant'
 alias hasslog='tail -f ~/.homeassistant/home-assistant.log'
 EOF
+
+
+# Install on CoreElec
+installentware
+opkg update
+opkg install gcc python3-pip python3-setuptools
+
+# Install cryptography
+wget http://launchpadlibrarian.net/230019290/libffi-dev_3.2.1-4_arm64.deb # https://launchpad.net/ubuntu/xenial/arm64/libffi-dev/3.2.1-4
+scp usr/include/ffitarget.h none:/opt/include
+scp usr/include/ffitarget.h none:/opt/include
+wget http://launchpadlibrarian.net/413028779/libssl-dev_1.0.2g-1ubuntu4.15_arm64.deb #https://launchpad.net/ubuntu/xenial/arm64/libssl-dev/1.0.2g-1ubuntu4.15
+#copy usr/include/openssl/* none:/opt/include/openssl
+#copy usr/include/aarch64-linux-gnu/openssl/* none:/opt/include/openssl
+
+ln -s /opt/lib/libffi.so.6.0.4 /opt/lib/libffi.so
+ln -s libcrypto.so.1.1 libcrypto.so
+ln -s libssl.so.1.1 libssl.so
+#pip3 install cryptography
+
+pip3 install wheel homeassistant
+
+systemctl mask kodi
+systemctl start kodi
+systemctl stop kodi
